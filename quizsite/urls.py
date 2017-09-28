f05590772f6f5e758ee3d1652fd7baa82fa5bd7e
hmetsis <32261630+hmetsis@users.noreply.github.com>
2017-09-28 11:23:14 +0200

    Started django admin interface

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 from quiz import views
 
@@ -22,5 +22,5 @@ urlpatterns = [
 	path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
 	path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
-
+    path('admin/', admin.site.urls),
 	]
