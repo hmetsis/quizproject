2a2e0238db799bd37df5159e95bb7f7b0a42156d
hmetsis <32261630+hmetsis@users.noreply.github.com>
2017-09-28 13:19:13 +0200

    commented out test data and changed context variable

--- quiz/views.py
+++ quiz/views.py
@@ -1,26 +1,27 @@
+from quiz.models import Quiz
 from django.shortcuts import render
 
-quizzes = [
-	{
-		"quiz_number": 1,
-		"name": "Klassiska böcker",
-		"description": "Hur bra kan du dina klassiker?"
-	},
-	{
-		"quiz_number": 2,
-		"name": "Största fotbollslagen",
-		"description": "Kn du dina lag?"
-	},
-	{
-		"quiz_number": 3,
-		"name": "Världens mest kända hackare",
-		"description": "Kan du din hackerhistoria?"
-	},
-]
+# quizzes = [
+# 	{
+# 		"quiz_number": 1,
+# 		"name": "Klassiska böcker",
+# 		"description": "Hur bra kan du dina klassiker?"
+# 	},
+# 	{
+# 		"quiz_number": 2,
+# 		"name": "Största fotbollslagen",
+# 		"description": "Kn du dina lag?"
+# 	},
+# 	{
+# 		"quiz_number": 3,
+# 		"name": "Världens mest kända hackare",
+# 		"description": "Kan du din hackerhistoria?"
+# 	},
+# ]
 
 def startpage(request):
 		context = {
-			"quizzes": quizzes,
+			"quizzes": Quiz.objects.all(),
 		}
 		return render(request, "start.html", context)
 
