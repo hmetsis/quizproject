081eade2a697a8fd017584542291f170083a0a55
hmetsis <32261630+hmetsis@users.noreply.github.com>
2017-09-28 13:28:44 +0200

    fixed indent error

--- quiz/views.py
+++ quiz/views.py
@@ -33,9 +33,9 @@ def quiz(request, quiz_number):
 		return render(request, "quiz.html", context)
 
 def question(request, quiz_number, question_number):
-	quiz = Quiz.objects.get(quiz_number=quiz_number)
-	questions = quiz.questions.all()
-	question = questions[quiz_number - 1]
+		quiz = Quiz.objects.get(quiz_number=quiz_number)
+		questions = quiz.questions.all()
+		question = questions[quiz_number - 1]
 		context = {
 			"question_number": question_number,
 			"question": question.question,
