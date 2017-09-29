d9ee91653ec64c9cb276468888489cc7b773f605
hmetsis <32261630+hmetsis@users.noreply.github.com>
2017-09-29 10:08:40 +0200

    edited databeases

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%x!rb+k%3s+j@d17o24x@ov62*u#-xnr*t9x(vy4!850r31p60'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,12 +74,17 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+
+
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(
+            os.path.join(BASE_DIR, 'db.sqlite3')
+        )
+    )
 }
+
 
 
 # Password validation
